a514847019ccb4bce1b2e71da2a942650ab7f94a
GiovanniLafortezza <recordeknot388@gmail.com>
2024-01-04 15:25:12 +0100

    add class Song

---
+++ src/main/java/org/lessons/java/bestoftheyear/model/Song.java
@@ -0,0 +1,42 @@
+package org.lessons.java.bestoftheyear.model;
+
+public class Song {
+    /* ------------ ATTRIBUTI ------------ */
+    private int id;
+    private String title;
+    private String author;
+
+    /* ------------ COSTRUTTORI ------------ */
+
+    public Song(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    /* ------------ GETTER E SETTER ------------ */
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
